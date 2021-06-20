e45f2d8a6a7158143fb4ef08bd6b54152ba057d1
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2021-06-20 15:12:49 +0200

    Edit users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->unsignedTinyInteger('housing_type')->nullable()->comment(' 1 => House, 2 => Apartment');
+            $table->unsignedTinyInteger('state_id')->nullable();
+            $table->string('house_number')->nullable();
+            $table->string('building_number')->nullable();
+            $table->string('floor_number')->nullable();
+            $table->string('apartment_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedTinyInteger('status')->default(1)->comment('0 => Inactive, 1 => Active, 2 => New');
+
             $table->rememberToken();
             $table->timestamps();
         });
